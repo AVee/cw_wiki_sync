8467c019219a1801c9f2ff0bf1fe58decbf2c473
AVee <github_cw@avee.org>
2019-11-16 16:05:09 +0100

    Make update find new items

--- src/update_resources.py
+++ src/update_resources.py
@@ -5,14 +5,26 @@ Created on 15 Nov 2019
 '''
 import mwclient
 from items import item
+from items.item import Item
 
 def run():
     site = mwclient.Site('chatwars-wiki.de', path='/')
 
+    # Refresh existing items
     items = item.load_from_file('../data/resources.json')
     for itm in items:
         itm.load(site)
     
+    # Find new items
+    pages = site.ask('[[ItemID::+]]')
+    
+    for page in pages:
+        name = page['fulltext']
+        if not any([i for i in items if i.pagename == name]):
+            itm = Item(pagename=name)
+            itm.load(site)
+            items.append(itm)
+
     item.save_to_file('../data/resources.json', items)
     
 if __name__ == '__main__':

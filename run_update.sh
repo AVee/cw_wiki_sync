f4ec489ddc6a46de4354b000e5b5142dacf09f38
Automagic <github_cw@avee.org>
2019-11-18 20:11:53 +0100

    Make script silent when nothing happens.

--- run_update.sh
+++ run_update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd /opt/wikisync/src
+pushd /opt/wikisync/src > /dev/null
 git pull --quiet
 python3 update_resources.py
 git add ../data/resources.json
-git commit -m "Automatic update of resources file."
-git push
-
+git diff --staged --exit-code || { git commit -m "Automatic update of resources file." && git push; }
+popd > /dev/null

29f2312d3f1c9fd770aa83cd42f72a31f677c5ee
Automagic <github_cw@avee.org>
2019-11-18 23:52:24 +0100

    Also commit lastrev on change.

--- run_update.sh
+++ run_update.sh
@@ -2,6 +2,6 @@
 pushd /opt/wikisync/src > /dev/null
 git pull --quiet
 python3 update_resources.py
-git add ../data/resources.json
+git add ../data/resources.json ../data/lastrev
 git diff --staged --exit-code || { git commit -m "Automatic update of resources file." && git push; }
 popd > /dev/null

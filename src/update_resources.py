814d2ec0e8b02acc80c669c9ab1381f787cf70f7
AVee <github_cw@avee.org>
2020-04-06 14:03:26 +0200

    Handle page move in update.

--- src/update_resources.py
+++ src/update_resources.py
@@ -23,25 +23,32 @@ def run():
     pages = None # Lazy loaded later only when needed
     lastrev = None
     
+    # Run through changed pages
     for change in changes:
         if not change['revid'] > maxrev:
             break
         if not lastrev:
             lastrev = change['revid']
+        
         page = next((i for i in items['items'] if i.pagename == change['title']), None)
-        if page:
+        
+        if page: # We had this item already, update.
             if page.revision < change['revid']:
                 page.load(site)
+                if not page._has_item: # No longer an item page (could be a page move/rename).
+                    items.remove()
         else:
             if not pages: # Lazy load list of item pages
                 pages = list(site.ask('[[ItemID::+]]'))
+            
             if any(p for p in pages if p['fulltext'] == change['title']):
                 itm = Item(change['title'])
                 itm.load(site)
-                if itm._has_item and any(i for i in items['items'] if i.ItemID == itm.ItemID):
-                    print(f"Duplicate Item {itm.ItemID} on {change['title']}, could be a renamed page.")
-                else:
-                    items['items'].append(itm)
+                
+                if not itm._has_item:
+                    continue
+                 
+                items['items'].append(itm)
 
     # Only save if there are changes
     if any(i for i in items['items'] if i.revision > maxrev):
@@ -54,20 +61,6 @@ def run():
         except Exception as e:
             print(f'Failed to save lastrev: {e}')
         
-#     for itm in items['items']:
-#         itm.load(site)
-#     
-#     # Find new items
-#     pages = site.ask('[[ItemID::+]]')
-#     
-#     for page in pages:
-#         name = page['fulltext']
-#         if not any([i for i in items['items'] if i.pagename == name]):
-#             itm = Item(pagename=name)
-#             itm.load(site)
-#             items['items'].append(itm)
-#
-#    item.save_to_file('../data/resources.json', items)
     
 if __name__ == '__main__':
     run()

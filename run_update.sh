91184b7824e8df49ae9326783d8a32eefdab1fc9
Automagic <github_cw@avee.org>
2020-04-06 16:13:47 +0200

    Stop commiting lastrev file when nothing else changed.

--- run_update.sh
+++ run_update.sh
@@ -3,6 +3,6 @@ pushd /opt/wikisync/src > /dev/null
 git pull --quiet
 python3 update_resources.py
 python3 generate_md.py
-git add ../data/resources.json ../data/lastrev ../docs/missingid.md ../docs/wikinotes.md
-git diff --staged --exit-code || { git commit -m "Automatic update of resources file." && git push; }
+git add ../data/resources.json ../docs/missingid.md ../docs/wikinotes.md
+git diff --staged --exit-code || { git add ../data/lastrev && git commit -m "Automatic update of resources file." && git push; }
 popd > /dev/null

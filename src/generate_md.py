5acc688abee46cdca84d7a2f9d0ba3533fc4f56b
AVee <github_cw@avee.org>
2020-04-06 15:16:45 +0200

    doc -> docs

--- src/generate_md.py
+++ src/generate_md.py
@@ -8,13 +8,13 @@ import io
 def run():
     items = item.load_from_file('../data/resources.json')
 
-    with io.open('../doc/missingid.md', 'w') as f:
+    with io.open('../docs/missingid.md', 'w') as f:
         f.write('## Items with missing IDs\n\n')
         
         for i in items['incomplete']:
             f.write(i.markdown_link() + '  \n')
         
-    with io.open('../doc/wikinotes.md', 'w') as f:
+    with io.open('../docs/wikinotes.md', 'w') as f:
         f.write('## Items with notes\n\n')
         
         for i in (items['items'] + items['incomplete']):

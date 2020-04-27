4f94460d40816b98c45b5b93dcbbd3984e7011d9
Automagic <github_cw@avee.org>
2020-04-27 22:49:42 +0200

    Fix run script.

--- run_update.sh
+++ run_update.sh
@@ -3,6 +3,6 @@ pushd /opt/wikisync/src > /dev/null
 git pull --quiet
 python3 update_resources.py
 python3 -m pagegenerators
-git add ../data/resources.json ../docs/missingid.md ../docs/wikinotes.md
+git add ../data/resources.json 
 git diff --staged --exit-code || { git add ../data/lastrev && git commit -m "Automatic update of resources file." && git push; }
 popd > /dev/null

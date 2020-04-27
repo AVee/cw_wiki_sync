a85a8b2a91718002956d86b6768555f6403bad44
Automagic <github_cw@avee.org>
2020-04-27 20:59:59 +0200

    Update run script to use pagegenerators module.

--- run_update.sh
+++ run_update.sh
@@ -2,7 +2,7 @@
 pushd /opt/wikisync/src > /dev/null
 git pull --quiet
 python3 update_resources.py
-python3 generate_md.py
+python3 -m pagegenerators
 git add ../data/resources.json ../docs/missingid.md ../docs/wikinotes.md
 git diff --staged --exit-code || { git add ../data/lastrev && git commit -m "Automatic update of resources file." && git push; }
 popd > /dev/null

7125b0b716500201649cba22fc5e14605bb0f5b7
AVee <github_cw@avee.org>
2020-04-27 20:56:15 +0200

    Add main entry point for pagegenerators module

---
+++ src/pagegenerators/__main__.py
@@ -0,0 +1,12 @@
+'''
+Created on 27 Apr 2020
+
+@author: AVee
+'''
+from pagegenerators import items_needing_attention
+
+def run_all():
+    items_needing_attention.run();
+
+if __name__ == '__main__':
+    run_all()

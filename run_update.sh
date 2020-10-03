3472069947f4dcccdf0d30f6c0944abe4526db1d
AVee <github_cw@avee.org>
2020-10-03 12:53:56 +0200

    Add v2 file...

--- run_update.sh
+++ run_update.sh
@@ -3,6 +3,6 @@ pushd /opt/wikisync/src > /dev/null
 git pull --quiet
 python3 update_resources.py
 python3 -m pagegenerators
-git add ../data/resources.json 
+git add ../data/resources.json ../data/resources_v2.json 
 git diff --staged --exit-code || { git add ../data/lastrev && git commit -m "Automatic update of resources file." && git push; }
 popd > /dev/null
